af68848303b2f14dc0d079f293bd65f26a96aa22
ximing <807754634@qq.com>
2014-07-27 16:09:59 +0800

    ajax

--- FenXiao.Web/Controllers/HomeController.cs
+++ FenXiao.Web/Controllers/HomeController.cs
@@ -49,7 +49,17 @@ namespace FenXiao.Web.Controllers
                 return View(page);
             }
         }
-
+        public string Ajax()
+        {
+            int count = 0;
+            for (int i = 0; i < 10000000; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                    count += j;
+                count += i * 2;
+            }
+            return count.ToString();
+        }
         public string Upload()
         {
             var filecollection = Request.Files;
