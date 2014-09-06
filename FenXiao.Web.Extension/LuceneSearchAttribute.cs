8b30ff6e2fe3ffdd9803a7f0f8b1e863cb18e577
ximing <807754634@qq.com>
2014-09-06 16:42:50 +0800

    lucene

---
+++ FenXiao.Web.Extension/LuceneSearchAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FenXiao.Web.Extension
+{
+    public class LuceneSearchAttribute : Attribute 
+    {
+        public LuceneSearchAttribute()
+        { }
+        private bool isIndex;
+        public bool IsIndex
+        {
+            get { return isIndex; }
+            set { isIndex = value; }
+        }
+        private bool isAdd;
+        public bool IsAdd
+        {
+            get { return isAdd; }
+            set { isAdd = value; }
+        }
+    }
+}
