d58037064554ea68cf639603698d87329ca237c9
wang1993 <329873230@qq.com>
2014-10-07 01:08:39 +0800

    报表导出+消息分类

--- FenXiao.Web/Areas/Marketer/Controllers/MHomeController.cs
+++ FenXiao.Web/Areas/Marketer/Controllers/MHomeController.cs
@@ -3,64 +3,12 @@ using FenXiao.Web.Common;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
-using FenXiao.Web.Models; 
+using FenXiao.Web.Models;
+using Webdiyer.WebControls.Mvc;
 namespace FenXiao.Web.Areas.Marketer.Controllers
 {
     public class MHomeController : MarketerControllerBase
     {
-        #region 消息管理
-        //消息列表
-        public ActionResult Message()
-        {
-            var msgs = db.Messages.Where(a => a.ToCompanyId == FenXiaoUserContext.Current.LoginInfo.CompanyId
-                && a.IsRead == 0 &&
-                (a.State == (int)EnumMessage.chulidingdan ||
-                a.State == (int)EnumMessage.chulituidan ||
-                a.State == (int)EnumMessage.chulipifashangshenqing)).ToList();
-            return View(msgs);
-        }
-
-        public ActionResult Read(int id)
-        {
-            var message = db.Messages.Find(id);
-            if (message == null)
-            {
-                return HttpNotFound();
-            }
-            else
-            {
-                message.IsRead = 1;
-                db.Entry(message).State = System.Data.Entity.EntityState.Modified;
-                db.SaveChanges();
-                if (message.State == (int)EnumMessage.chulidingdan)
-                {
-                    return RedirectToAction("CommonOrderDetail", "MHome", new { Area = "Marketer", id = message.RelatedId });
-
-                }
-                else if (message.State == (int)EnumMessage.chulituidan)
-                {
-                    return RedirectToAction("CancelOrderDetail", "MHome", new { Area = "Marketer", id = message.RelatedId });
-
-                }
-                else
-                {
-                    return RedirectToAction("HandleComInfoResult", "MHome", new { Area = "Marketer", id = message.RelatedId });
-
-                }
-            }
-        }
-
-        //公司申请信息结果
-        public ActionResult HandleComInfoResult(int id)
-        {
-            var v = db.HandleApplies.Find(id);
-            if (v==null)
-            {
-                return HttpNotFound();
-            }
-            return View(v);
-        }
-        #endregion
 
         public ActionResult Help()
         {
