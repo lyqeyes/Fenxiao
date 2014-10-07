fd77db9fa054fcdb79bb0193c021d626db6d9f19
wang1993 <329873230@qq.com>
2014-10-07 18:06:41 +0800

    添加权限枚举

---
+++ FenXiao.Model/EnumMessageReadState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yeanzhi.Framework.Utility;
+
+namespace FenXiao.Model
+{
+    /// <summary>
+    /// 消息阅读状态枚举
+    /// </summary>
+    public enum EnumMessageReadState
+    {
+        /// <summary>
+        /// 已读
+        /// </summary>
+        [EnumTitle("已读", IsDisplay = false)]
+        Read = 1,
+        /// <summary>
+        /// 未读
+        /// </summary>
+        [EnumTitle("未读", IsDisplay = false)]
+        NotRead = 0
+    }
+}
