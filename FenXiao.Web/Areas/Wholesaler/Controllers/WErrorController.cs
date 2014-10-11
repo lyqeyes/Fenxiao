ca5df3ab2b2bc91dbc8a6d5b80fe1afd63e28600
ximing <807754634@qq.com>
2014-10-11 10:59:16 +0800

    cuowuchuli

---
+++ FenXiao.Web/Areas/Wholesaler/Controllers/WErrorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FenXiao.Web.Areas.Wholesaler.Controllers
+{
+    public class WErrorController : Controller
+    {
+
+        public ActionResult HttpNotFound()
+        {
+            return View();
+        }
+
+        public ActionResult BadRequestt()
+        {
+            return View();
+        }
+
+        public ActionResult NoPermission()
+        {
+            return View();
+        }
+    }
+}
